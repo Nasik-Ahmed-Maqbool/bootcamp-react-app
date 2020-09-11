f5513e99860a2bd7cd26744f27af74a1d9cd08fd
Nasik-Ahmed-Maqbool <ahmednasik085@gmail.com>
2020-09-12 00:43:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Dinner from './dinner'
 function App() {
   return (
     <div className="App">
-      <h1>how is it going on</h1>
+     <Dinner /> 
      
     </div>
   );

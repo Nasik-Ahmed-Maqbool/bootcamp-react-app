333fb9afc5d3da9c5f1030ecf9910997c5ee78ff
Nasik-Ahmed-Maqbool <ahmednasik085@gmail.com>
2020-09-12 00:30:43 +0500

    update dinner.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Dinner from "./dinner"
+import Dinner from "./dinner.js"
 function App() {
   return (
     <div className="App">
-      <h1>Hello World From Nasik</h1>
+      
       <Dinner />
     </div>
   );

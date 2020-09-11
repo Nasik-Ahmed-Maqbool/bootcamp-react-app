a9332a3eca20baf401669e390b9a9f13c1b19f98
Nasik-Ahmed-Maqbool <ahmednasik085@gmail.com>
2020-09-12 01:28:19 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -5,6 +5,6 @@ function Dinner(props){
 
         <h1>today we have to serve {props.dishname}</h1>
         <h1>today we also have to serve {props.sweetdish}</h1>
-    )
+    );
 }
 export default Dinner;

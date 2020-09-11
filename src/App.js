7e60a670b70a9af3138e21bb7f7c8081215da22a
Nasik-Ahmed-Maqbool <ahmednasik085@gmail.com>
2020-09-12 00:27:02 +0500

    made a js file to run

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Dinner from "./dinner"
 function App() {
   return (
     <div className="App">
       <h1>Hello World From Nasik</h1>
+      <Dinner />
     </div>
   );
 }
---
+++ src/dinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function()
+{
+    return(
+        <h1>today we are serving chicken biryani</h1>
+<h1>as well as korma too</h1>
+
+
+
+    )
+
+
+
+
+}
